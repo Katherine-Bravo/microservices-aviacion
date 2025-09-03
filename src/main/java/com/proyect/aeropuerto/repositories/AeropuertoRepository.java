7b9d0d7c20c3868c33dd85143d59686423fbd618
Katherine091298 <rkbm.2300536568@gmail.com>
2025-09-02 21:55:06 -0500

    Proyecto completo: backend y frontend

--- src/main/java/com/proyect/aeropuerto/repositories/AeropuertoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.proyect.aeropuerto.repositories;
-
-import com.proyect.aeropuerto.models.AeropuertoModelo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AeropuertoRepository extends JpaRepository<AeropuertoModelo, Long> {
-}
--- src/main/java/com/proyect/aeropuerto/repositories/AvionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.proyect.aeropuerto.repositories;
-
-import com.proyect.aeropuerto.models.AvionModelo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AvionRepository extends JpaRepository<AvionModelo, Long> {
-}
